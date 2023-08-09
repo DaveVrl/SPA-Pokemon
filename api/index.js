36fcd83464ae557cdada64edf16fa4660d3060f4
Dave <doble.aguila@hotmail.com>
2023-08-09 14:51:30 -0300

    Cambios para Deploy

--- api/index.js
+++ api/index.js
@@ -20,11 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const getType = require('./src/controllers/getType.js')
+const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async() => {
   await getType();
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
